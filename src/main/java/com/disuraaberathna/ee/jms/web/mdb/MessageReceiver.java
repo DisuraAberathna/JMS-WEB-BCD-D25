d0a6b79c546f239006076c6c234ab0883ea62c5f
Disura Aberathna <disura2005@gmail.com>
2025-05-08 10:45:50 +0530

    update

--- src/main/java/com/disuraaberathna/ee/jms/web/mdb/MessageReceiver.java
+++ src/main/java/com/disuraaberathna/ee/jms/web/mdb/MessageReceiver.java
@@ -1,5 +1,6 @@
 package com.disuraaberathna.ee.jms.web.mdb;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.ejb.ActivationConfigProperty;
 import jakarta.ejb.MessageDriven;
 import jakarta.jms.JMSException;
@@ -7,13 +8,22 @@ import jakarta.jms.Message;
 import jakarta.jms.MessageListener;
 
 @MessageDriven(activationConfig = {
-        @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "jms/MyTopic")
+//        @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "jms/MyTopic")
+        @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "jms/MyQueue"),
+//        @ActivationConfigProperty(propertyName = "maxSession", propertyValue = "1")  // Server-specific
+        @ActivationConfigProperty(propertyName = "maxPoolSize", propertyValue = "1")
 })
 public class MessageReceiver implements MessageListener {
+
+    @PostConstruct
+    public void init() {
+        System.out.println("MessageReceiver init...");
+    }
+
     @Override
     public void onMessage(Message message) {
         try {
-            System.out.println("Received message: " + message.getBody(String.class));
+            System.out.println(this + "Received message: " + message.getBody(String.class));
         } catch (JMSException e) {
             throw new RuntimeException(e);
         }
